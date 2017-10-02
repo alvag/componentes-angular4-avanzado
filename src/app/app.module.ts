22eefd3611957a5f21640c319063ed04cfa6beda
Max Alva <alva85@gmail.com>
2017-10-01 20:21:47 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { TimerComponent } from './timer/timer.component';
+import { DisplayComponent } from './display/display.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProgressBarComponent,
+    TimerComponent,
+    DisplayComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {}
